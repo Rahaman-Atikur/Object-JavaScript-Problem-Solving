312573d1c918a3bd84d3537748fb5b0159012067
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-05 21:50:39 +0100

    First commit

---
+++ string.js
@@ -0,0 +1,12 @@
+const country ='Bangladesh';
+const division = 'Dhaka';
+const district = 'Manikganj';
+const thana = new String('Drama');
+console.log(typeof thana);
+
+const numbers = [54,22,14,78,66];
+console.log(numbers.length);
+console.log(numbers[2]);
+
+const capital = 'Bangladesh';
+console.log(capital.length);
