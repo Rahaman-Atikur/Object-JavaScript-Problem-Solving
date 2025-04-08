0162645fb9217d5506ef338b88f942b23200b4c3
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-08 19:30:09 +0100

    Solved problem no 4

--- stringTask4.js
+++ stringTask4.js
@@ -2,17 +2,14 @@
 If a given string has either x, replace x by y. if the given string has X, replace it by Y.
 
 Hint: You should be able to check whether x or X exists. After that, search online how to replace a character in a string.*/
-let myString = 'Xxyy';
-let result;
-for(let i=0;i<myString.length;i++){
-    if(myString[i]=='x'){
-        result = myString.replace('x','y');
+let myString = 'Xxxxxxxxx';
+for (let i = 0; i < myString.length; i++) {
+    if (myString.includes('x')) {
+        myString = myString.replaceAll('x', 'y');
     }
-
-    if(myString[i]=='X'){
-        //replace it by Y
-        result = myString.replace('X','Y');
+    if (myString.includes('X')) {
+        myString = myString.replaceAll('X', 'Y');
     }
-
 }
-console.log(result);
+
+console.log(myString);
