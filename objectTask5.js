dd3dc10254211feef6668423e11dbf132ca9185c
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-08 07:26:36 +0100

    last one almost done

---
+++ objectTask5.js
@@ -0,0 +1,31 @@
+/*ask-5 (Hard)
+Loop through an object and print the key-value pairs with their types
+
+Input:
+
+let myObject = {
+
+name: 'John Doe',
+age: 25,
+city: 'Example City',
+isStudent: true
+};
+
+Output:
+
+
+key: name | type:  string
+key: age | type:  number
+key: city | type:  string
+key: isStudent | type:  boolean
+*/
+let myObject = {
+    name: 'Atikur',
+    age: 23,
+    city: 'Dhaka',
+    isStudent: true
+}
+
+for (let val in myObject) {
+    console.log('key'+' ' + ':' +' ' + val +' '+ '|'+' '+ typeof(val));
+}
