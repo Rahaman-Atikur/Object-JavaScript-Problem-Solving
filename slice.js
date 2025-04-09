46ca209e5d5d3a98146182396a264c26e75dab35
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-09 11:44:18 +0100

    join and split done

---
+++ slice.js
@@ -0,0 +1,13 @@
+const address = 'Bangladesh';
+const partOfAddress = address.slice(3,7);
+console.log(partOfAddress);
+
+const sentence = "I am a hardworking person";
+console.log(sentence.split('I'));
+
+const friendsString = 'Rahim,karim,okToLight';
+const friends = friendsString.split(',');
+console.log(friends);
+
+const realFriends = ['Rahim','Karim','Jashim','Nadim'];
+console.log(realFriends.join('-'));
