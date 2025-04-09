3fe817f4509b5a49fd6173405590e0a3fa5034bd
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-09 16:20:28 +0100

    Capitalize word isnot solved yet

--- stringTask5.js
+++ stringTask5.js
@@ -1,3 +1,10 @@
 /*Task-5:
 Capitalize Every first Letter of each word in a String
 */
+let givenString = 'shuvo';
+for(let i =0 ;i<givenString.length;i++){
+    if(givenString[0]=='s'){
+        givenString[0].replaceAll('s','S');
+    }
+}
+console.log(givenString);
